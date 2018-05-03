351b67144ead75f015c48c7dd8dedbc0c699669d
张楠 <zhangnan@hualala.com>
2018-05-03 09:19:56 +0800

    have some fun

---
+++ pages/home/home.js
@@ -0,0 +1,80 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    banners: [{
+      id: 3,
+      img: 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      name: '百亿巨惠任你抢'
+    },
+    {
+      id: 1,
+      img: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      name: '告别午高峰'
+    },
+    {
+      id: 2,
+      img: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      name: '金牌好店'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
