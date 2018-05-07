76e2309a1b5b929ac45feccfc3513d5838d79eff
zhangnan007 <https://zhangnan007@github.com>
2018-05-08 00:12:05 +0800

    0:11 commit fun code

--- pages/searchShop/searchShop.js
+++ pages/searchShop/searchShop.js
@@ -8,36 +8,32 @@ Page({
     currentTab:0,
     nearNum:10,
     collectNum:0,
+    currentShop:0,
     shops:[{
       address:"西环嘉茂",
       distance:"117米",
       fullAddress:"北京市西城区西直门外大街1号西区商业'凯德MALL'嘉茂购物中心位于西环广场地下一层",
-      collectSrc:"../img/weishoucang.png",
-      checked:true
+      isCollect:0
     },{
         address: "西直门嘉茂",
         distance: "120米",
         fullAddress: "北京市西城区西直门南大街甲15-6",
-        collectSrc: "../img/weishoucang.png",
-        checked: false
+        isCollect: 1
     },{
         address: "展览路",
         distance: "300千米",
         fullAddress: "北京市西城区西外大街得宝新园14号",
-        collectSrc: "../img/weishoucang.png",
-        checked: false
+        isCollect: 0
     },{
         address: "车公庄",
         distance: "301千米",
         fullAddress: "北京市西城区车公庄大街丙4号一层+二层",
-        collectSrc: "../img/weishoucang.png",
-        checked: false
+        isCollect: 0
     },{
         address: "积水潭",
         distance: "303千米",
         fullAddress: "北京市西城区新街口北大街1号1号楼一层",
-        collectSrc: "../img/weishoucang.png",
-        checked: false
+        isCollect: 0
     }]
   },
   /**
@@ -60,6 +56,29 @@ Page({
     var that = this;
     that.setData({
       currentTab: e.detail.current
+    });
+  },
+  /**
+   * 店铺列表单选切换事件
+   */
+  shoplistChange:function(e){
+    var that = this;
+    that.setData({
+      currentShop: e.detail.value
+    });
+  },
+  /**
+   * 收藏店铺点击事件
+   */
+  onClickCollectImg:function(e){
+    var that = this;
+    var index = e.currentTarget.id;
+    var shopDetial = that.data.shops[index];
+    shopDetial.isCollect = shopDetial.isCollect==0?1:0;
+    that.data.shops[index] = shopDetial;
+    that.setData({
+      currentShop: index,
+      shops:that.data.shops
     });
   },
   /**
