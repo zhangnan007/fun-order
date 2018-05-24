d199b93d442bf57ed43f2e3306426806e40c2497
zhangnan007 <https://zhangnan007@github.com>
2018-05-25 00:26:35 +0800

    20180525 commit

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -10,7 +10,19 @@ Page({
     foodTypes: [],
     foods: []
   },
-
+  showFoodDetail: function (event) {
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
