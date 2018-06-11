2a63df3c2ac62e78c39a54714ec3a190a5a6c6f8
zhangnan007 <https://zhangnan007@github.com>
2018-06-11 23:01:04 +0800

    food type and food detail linkage

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -4,30 +4,78 @@ Page({
    * 页面的初始数据
    */
   data: {
+    currentFoodTitle:"foodTitle" + 0, // 当前选中菜品类型
+    currentFood: "food" + 0, // 当前选中右侧菜品详情
+    foodHeiCount: 0,// 右侧区域总高度
     totalPrice:0,
     foodNum:0,
     shopCartCount:0,
     banners: [],
     foodTypes: [],
-    foods: []
+    foods: [],
+    foodsHeights:[] // 菜单右侧分类高度
   },
   /**
-   * 菜品左侧菜品分类点击事件
+ * 右侧菜单明细滚动事件
+ */
+  foodOnScroll: function (event) {
+    // 锁定上下文
+    let that = this;
+    // 获取右侧计算好的菜品高度
+    var foodhs = that.data.foodsHeights;
+    // 右侧菜品信息数据占用总高度
+    var heiCount = that.data.foodHeiCount;
+    // 累计高度
+    var num = 0;
+    // 选中菜品类型索引
+    var checkedFoodTypeIndex;
+    // 循环菜品高度
+    for(var index in foodhs){
+      var hitem = foodhs[index];
+      if (num == 0) {
+        num = hitem;
+      }
+      var scrollTop = event.detail.scrollTop;
+      if (num < scrollTop){
+        if (index == 0){
+          continue;
+        }
+        num = num + parseFloat(hitem);
+        if (num >= scrollTop){
+          checkedFoodTypeIndex = index;
+          break;
+        }
+      } else {
+        checkedFoodTypeIndex = index;
+        break;
+      }
+    }
+    console.log("height: " + num + " scroll top: " + scrollTop);
+    console.log("checked index: " + checkedFoodTypeIndex);
+    // 修改左侧菜品分类顶端位置
+    that.setData({
+      currentFood: "food" + checkedFoodTypeIndex
+    });
+    // 调用修改菜单分类选中样式函数
+    that.updateLeftFoodTypeChecked(checkedFoodTypeIndex);
+  },
+  /**
+   * 修改左侧菜品类型选中样式
    */
-  foodTypeOnClick: function(event){
+  updateLeftFoodTypeChecked(checkedIndex){
     let that = this;
     // 当前元素下标
-    let index = parseInt(event.currentTarget.id);
+    var index = parseInt(checkedIndex);
     // 后一项元素下标
     var afIndex = 1 + index;
     // 前一项元素下标
-    var beIndex = currIndex === 0 ? 0 : index - 1;
+    var beIndex = index === 0 ? 0 : index - 1;
     var tempFoodTypes = that.data.foodTypes;
-    for (var currIndex in tempFoodTypes){
+    for (var currIndex in tempFoodTypes) {
       // 当前元素
       var item = tempFoodTypes[currIndex];
       // 设置备选元素前后节点border样式
-      if (currIndex == beIndex){
+      if (currIndex == beIndex) {
         item.isUBottom = true;
         item.isUTop = false;
       } else if (currIndex == afIndex) {
@@ -38,16 +86,33 @@ Page({
         item.isUTop = false;
       }
       // 设置被选元素样式
-      if (currIndex == index){
+      if (currIndex == index) {
         item.isChecked = true;
-      } else if (item.isChecked){
+      } else if (item.isChecked) {
         item.isChecked = false;
       }
-    }
-    this.setData({
+    };
+    that.setData({
       foodTypes: tempFoodTypes
     });
+    return {
+      currentFoodTitle: "foodTitle" + index
+    };
   },
+  /**
+   * 菜品左侧菜品分类点击事件
+   */
+  foodTypeOnClick: function(event){
+    let that = this;
+    var index = event.currentTarget.dataset.index;
+    var data = that.updateLeftFoodTypeChecked(index);
+    that.setData({
+      currentFoodTitle: data.currentFoodTitle
+    });
+  },
+  /**
+   * 展示菜品详情
+   */
   showFoodDetail: function (event) {
     wx.showModal({
       title: '提示',
@@ -110,11 +175,42 @@ Page({
       fail:function(){
         // 提示获取菜品失败
       },complete:function(){
+        // 隐藏动画加载中效果
         wx.hideToast();
+        // 为菜单类型和菜品明细联动做准备
+        // 1.获取设备屏幕高度
+        wx.getSystemInfo({
+          success: function (result) {
+            var winHeight = result.windowHeight * 0.69; // 获取屏幕可用高度
+            // 获取右侧菜品分类部分的高度3%
+            var foodTitleHeight = Number(winHeight * 0.03);
+            // 获取右侧菜品详情部分单个菜品的高度20% 
+            var foodDetailHeight = Number(winHeight * 0.2);
+            // 声明临时容器
+            var heights = new Array();
+            // 获取菜品详情数据
+            var foods = that.data.foods;
+            var heiCount = that.data.foodHeiCount;
+            // 循环右侧菜品详情
+            for (var index in foods) {
+              var food = foods[index];
+              // 计算右侧单个菜品类型的总高度(N个菜品详情+1个菜品类型标题)
+              var conHeight = Number(food.foodsIntro.length * foodDetailHeight + foodTitleHeight);
+              // 记录到集合容器中，顺序与左侧菜品类型保持一致
+              heights.push(conHeight);
+              // 计算总高度
+              heiCount = Number(heiCount + conHeight);
+            };
+            // 存储全局数据
+            that.setData({
+              foodsHeights: heights,
+              foodHeiCount: heiCount
+            });
+          }
+        });
       }
     });
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
