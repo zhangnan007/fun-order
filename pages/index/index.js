fcfde90e51ce7f6a83b5df0b35fc0919a280f913
zhangnan007 <https://zhangnan007@github.com>
2018-09-18 21:59:19 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,41 +1,40 @@
-// pages/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    isPacking:false, // 是否被选中 
+    isPacking: false, // 是否被选中 
     banners: [],
     nearShops: [],
   },
   /**
    * 店铺列表点击事件
    */
-  shopOnclick:function(event){
+  shopOnclick: function (event) {
     var that = this;
     var diningTypeCode = 1;
-    if (that.data.isPacking){
+    if (that.data.isPacking) {
       diningTypeCode = 2;
     }
     var groupId = event.currentTarget.dataset.groupid;
     var shopId = event.currentTarget.dataset.shopid;
     var list = that.data.banners;
     wx.navigateTo({
-      url: '../menu/menu?gId=' + groupId + '&sId=' + shopId + '&banners=' + list + '&type=' + diningTypeCode,
+      url: '../menu/menu?gId=' + groupId + '&sId=' + shopId + '&type=' + diningTypeCode,
     })
   },
   /**
    * 扫码点餐
    */
-  scanTowCode:function(event){
+  scanTowCode: function (event) {
     // 拉起微信扫码
     wx.scanCode({
       // onlyFromCamera: true,
-      scanType:"qrCode",
-      success:function(result){
+      scanType: "qrCode",
+      success: function (data) {
         wx.navigateTo({
-          url:'../menu/menu'
+          url: '../menu/menu?content=' + data.result,
         });
       }
     });
@@ -43,10 +42,10 @@ Page({
   /**
    * 切换就餐类型选中样式
    */
-  changeStyle:function(event){
+  changeStyle: function (event) {
     var that = this;
     var temPack = that.data.isPacking;
-    if (temPack){
+    if (temPack) {
       temPack = false;
     } else {
       temPack = true;
@@ -58,23 +57,50 @@ Page({
   /**
    * 不支持提示
    */
-  notSupportHint: function(){
+  notSupportHint: function () {
     wx.showModal({
-      title:"提示",
-      content:"暂不支持该就餐类型",
-      showCancel:false,
-      confirmText:"知道了",
+      title: "提示",
+      content: "暂不支持该就餐类型",
+      showCancel: false,
+      confirmText: "知道了",
       confirmColor: "#d81e06"
     })
   },
   /**
-   * 生命周期函数--监听页面加载
+   * 获取openId
+   * */
+  getOpenId: function () {
+    wx.login({
+      success: function (data) {
+        console.log(data)
+        if (data.code) {
+          wx.request({
+            url: 'http://localhost:8080/reqThird/getOpenId',
+            method: 'post',
+            header: {
+              'content-type': 'application/x-www-form-urlencoded'
+            },
+            data: {
+              code: data.code
+            },
+            success: function (data) {
+              console.log(data);
+              if (data.code == 0) {
+                wx.setStorageSync('funOrderOpenIdKey', data.data);
+              }
+            }
+          })
+        }
+      }
+    });
+  },
+
+  /**
+   * 获取首页banner
    */
-  onLoad: function (options) {
-    var that = this;
-    // 获取banners图
+  getIndexBanner: function (that) {
     wx.request({
-      url: 'https://easy-mock.com/mock/5afb0355fa8a1e7a7397ec92/example/findBanners',
+      url: 'http://localhost:8080/platform/getBanners',
       method: 'POST',
       header: {
         'content-type': 'application/json' // 默认值
@@ -86,25 +112,50 @@ Page({
       },
       fail: function (res) {
       }
-    })
-    // 获取附近餐厅
-    wx.request({
-      url: 'https://easy-mock.com/mock/5afb0355fa8a1e7a7397ec92/example/queryNearShops',
-      method: 'POST',
-      header: {
-        'content-type': 'application/json' // 默认值
+    });
+  },
+  /**
+   * 获取用户当前位置 
+   */
+  getCurrentLocation: function (that) {
+    wx.getLocation({
+      success: function (data) {
+        console.log(data);
+        // 获取附近餐厅
+        wx.request({
+          url: 'http://localhost:8080/platform/getNearShops',
+          data: {
+            latitude: data.latitude,
+            longitude: data.longitude
+          },
+          method: 'post',
+          header: {
+            'content-type': 'application/x-www-form-urlencoded'
+          },
+          success: function (res) {
+            that.setData({
+              nearShops: res.data.data,
+            });
+          },
+          fail: function (res) {
+          }
+        })
       },
-      data: {
-        currentPosition: 111
-      },
-      success: function (res) {
-        that.setData({
-          nearShops: res.data.data,
-        });
-      },
-      fail: function (res) {
+      fail: function (data) {
       }
-    })
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    // 获取首页banner图
+    that.getIndexBanner(that);
+    // 获取当前位置
+    that.getCurrentLocation(that);
+    // 获取并缓存openId
+    that.getOpenId();
   },
 
   /**
